8afeb33f23c39414db7bfc52f9c0700188b74cae
skynetcap <100323448+skynetcap@users.noreply.github.com>
2022-08-04 12:23:21 -0700

    initial commit

---
+++ src/main/java/io/openserum/SerumDatabaseApplication.java
@@ -0,0 +1,12 @@
+package io.openserum;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SerumDatabaseApplication {
+
+    public static void main(String[] args) {
+        log.info("Swag");
+    }
+
+}
